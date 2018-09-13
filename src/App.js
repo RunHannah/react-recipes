dce74921fb64c71c3626397bf2a2c6a4a6905ccd
Hannah Lee <leexhannah@gmail.com>
2018-09-13 11:38:13 -0700

    Add a submit button that gets recipe.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import "./App.css";
 import Form from "./components/form";
 
 class App extends Component {
+  //pass in e (for event) to prevent default page refresh
+  getRecipe = e => {
+    e.preventDefault();
+    console.log("working");
+  };
+
   render() {
     return (
       <div className="App">
@@ -10,7 +16,7 @@ class App extends Component {
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <h1 className="App-title">Recipe Directory</h1>
         </header>
-        <Form />
+        <Form getRecipe={this.getRecipe} />
       </div>
     );
   }
